60404bc1ffd1aa32c99ce48aae3bef1502683ff0
krishnakamith <krishnakamith@gmail.com>
2025-07-06 21:29:14 +0530

    added cbv : home-articles, create-article, update-article and delete-article

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import home, CreateArticleView
+from .views import (
+    ArticleListView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
 
 urlpatterns = [
-    path("", home, name='home'),
-    path("articles/create/", CreateArticleView.as_view(), name="create_article"),
+    path("", ArticleListView.as_view(), name='home'),
+    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
+    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
+    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article")
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from .models import Article
-from django.views.generic import CreateView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView,
+)
 
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "articles/home.html", {"articles" : articles})
+class ArticleListView(ListView):
+    template_name = "articles/home.html"
+    model = Article
+    context_object_name = "articles"
 
-class CreateArticleView(CreateView):
+class ArticleCreateView(CreateView):
     template_name = "articles/create_article.html"
     model = Article
     fields = ("title", "content", "status", "word_count", "twitter_post")
     success_url = reverse_lazy("home")
+
+class ArticleUpdateView(UpdateView):
+    template_name = "articles/update_article.html"
+    model = Article
+    fields = ("title", "content", "status", "word_count", "twitter_post")
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
+
+class ArticleDeleteView(DeleteView):
+    template_name = "articles/delete_article.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
