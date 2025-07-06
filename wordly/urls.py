72306cb33d4c06073fd6880d3e0743b582297c9c
krishnakamith <krishnakamith@gmail.com>
2025-07-06 21:37:41 +0530

    home page url update

--- wordly/urls.py
+++ wordly/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("articles.urls"))
+    path("articles/", include("articles.urls")),
+    path("", RedirectView.as_view(url="/articles/", permanent=False)),
 ]
