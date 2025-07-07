ecc15c2f32689563d18ee30e35cb42803e5bab2d
krishnakamith <krishnakamith@gmail.com>
2025-07-07 22:15:00 +0530

    Customized Article in admin plan

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from articles.models import UserProfile, Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "word_count", "status", "created_at", "updated_at")
+    list_filter = ("status",)
+    search_fields = ("title", "content")
+    date_hierarchy = "created_at"
+    ordering = ("created_at",)
+    readonly_fields = ("word_count", "created_at", "updated_at")
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(UserProfile)
